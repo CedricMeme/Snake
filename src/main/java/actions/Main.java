c0d9e9a67a1ac55c4c7a298a1842733ce39cf431
Greta <greta.sund@gmail.com>
2023-03-17 21:55:38 +0800

    Idea how you can introduce two different modes for data persistence using a command line argument

--- src/main/java/actions/Main.java
+++ src/main/java/actions/Main.java
@@ -5,6 +5,15 @@ import Music.Music;
 public class Main {
 
     public static void main(String[] args){
+        // Check if game should run with or without database
+        if(args != null && args[0] == "database") {
+            // Game should run with database mode
+            // Do something..
+        } else {
+            // Game will use a text file to save high scores
+            // Do something...
+        }
+
         Gui gui = new Gui();
         gui.menuStart();
         Music music = new Music();
